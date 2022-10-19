b0bb3c174f3d52693905ced41b480fcdd2dad9d9
Oguzhan YALCIN <oguzhanfinal@gmail.com>
2022-10-19 20:37:08 +0300

    .

--- Java/KDVTutari/src/App.java
+++ Java/KDVTutari/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Tutar Giriniz: ");
+        double tutar = input.nextDouble();
+        if(tutar>1000)
+        {
+            double kdv = tutar * 0.08;
+            System.out.println("KDV Tutarı: " + kdv);
+            tutar += kdv;
+            System.out.println("KDV'li Fiyat: " + tutar);
+        }
+        else
+        {
+            double kdv = tutar * 0.18;
+            System.out.println("KDV Tutarı: " + kdv);
+            tutar += kdv;
+            System.out.println("KDV'li Fiyat: " + tutar);
+        }
     }
 }
