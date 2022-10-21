a5058df7d1fa37c180fb728b53c3badb2911e838
Oguzhan YALCIN <oguzhanfinal@gmail.com>
2022-10-22 00:04:13 +0300

    .

--- Java/KullaniciGirisi/src/App.java
+++ Java/KullaniciGirisi/src/App.java
@@ -1,40 +1,20 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class App {
+    
+    static void number(int x, int z) {
+        int y = 2;
+        x = x * y;
+        System.out.println(x);
+    }
+    
+    static int as(int i) {
+        if (i < 2) return 1;
+        else return (i * as(i - 1));
+    }
+    
     public static void main(String[] args) throws Exception {
-        String inUsername, inPassword;
-        final String USERNAME = "patika", PASSWORD = "developer";
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Kullanıcı Adınız: ");
-        inUsername = input.next();
-
-        System.out.print("Şifreniz: ");
-        inPassword = input.next();
-
-        if(inUsername.equals(USERNAME) && inPassword.equals(PASSWORD))
-        {
-            System.out.print("Giriş Başarılı!");
-        }
-        else
-        {
-            System.out.println("Şifre Yanlış");
-            System.out.print("Şifrenizi Sıfırlamak İster misiniz? [Evet için 1 , Hayır içi 2]");
-            int secim = input.nextInt();
-            if(secim == 1){
-
-                System.out.print("Yeni Şifrenizi Giriniz: ");
-                String newPassword = input.next();
-                if(newPassword.equals(PASSWORD))
-                System.out.println("Yeni şifre eskisiyle aynı olamaz!");
-                else {
-                    System.out.println("Şifreniz Değiştirildi.");
-                    inPassword = newPassword;
-                }
-
-            }
-
-        }
-        input.close();
+        boolean a = true, b = false;
+System.out.println((a && b) || (a || b));
     }
 }
